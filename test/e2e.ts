e63207dbcae9d90752dc4f6bb6e4c8ac978514f5
t11s <greenfeatherhelp@gmail.com>
2021-01-28 21:18:17 -0800

    debug commit

--- test/e2e.ts
+++ test/e2e.ts
@@ -1,20 +1,30 @@
-// import { l2ethers } from "hardhat";
-// import { ethers } from "hardhat";
+import { SignerWithAddress } from "@eth-optimism/plugins/hardhat/ethers/internal/signer-with-address";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { solidity } from "ethereum-waffle";
+chai.use(solidity);
+chai.use(chaiAsPromised);
+chai.should();
 
-// import chai from "chai";
-// import chaiAsPromised from "chai-as-promised";
+import { ethers, l2ethers } from "hardhat";
 
-// import { L2Checkpoint__factory } from "../typechain";
+import { L2Checkpoint__factory } from "../typechain";
 
-// chai.use(chaiAsPromised);
-// chai.should();
+describe("Everything", function () {
+  let l1Signers;
+  let l2Signers;
 
-// describe("Everything", function () {
-//   it("should allow deposits on L2", async function () {
-//     const L2CheckpointFactory = (await l2ethers.getContractFactory(
-//       "L2Checkpoint"
-//     )) as L2Checkpoint__factory;
+  before(async () => {
+    l1Signers = await ethers.getSigners();
+    l2Signers = await l2ethers.getSigners();
+  });
 
-//     //TODO
-//   });
-// });
+  it("should allow deposits on L2", async function () {
+    let [l1Deployer, l1User] = l1Signers as SignerWithAddress[];
+    let [l2Deployer, l2User] = l2Signers as SignerWithAddress[];
+
+    const L2CheckpointFactory = (await l2ethers.getContractFactory(
+      "L2Checkpoint"
+    )) as L2Checkpoint__factory;
+  });
+});
